c811d5ffa60465d53978d264fe8ec1ef78bcba2d
Sayed Ali Kumail <42914002+alikumail@users.noreply.github.com>
2022-06-26 01:36:47 +0400

    upload theme

---
+++ components/about/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import DefaultModal from '../AboutModal'
+
+
+const About = (props) => {
+
+    return(
+        <section id="about" className="tp-about-section">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-lg-5 col-md-12 col-12">
+                        <div className="tp-about-wrap">
+                        <div className="tp-about-img">
+                            <img src="images/about.jpg" alt=""/>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-7 col-md-12 col-12">
+                        <div className="tp-about-text">
+                        <div className="tp-about-icon">
+                            <i className="fi flaticon-web-design"></i>
+                        </div>
+                        <div className="tp-about-icon-content">
+                            <h2>Professional And Dedicated Creative Designer</h2>
+                            <p>Check out 10 Best Design's updates for the top web design  & development companies for your needs by reviewing our list & development companies! Find the best web design. Web Design Consulting. Comprehensive Directory. Top Reviewed Design Firms. Types: Enterprise Design Firms, Startup Design Firms, Custom Design Firms, eCommerce Design Firms, App Design Firms. Custom Web Solution. 24x7 Customer Support. Secure Payment Gateway. Get A Free Web Quote. 24/7 Chat Support. Services: Web Page Design, Landing Page Design, App development, Mobile Website Design</p>
+                            <div className="signeture">
+                                <span><img src="images/signeture.png" alt=""/></span>
+                                <p>Ceo Of Designlab IT</p>
+                            </div>
+                            <DefaultModal buttonClass={'template-btn'}/>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="white_svg svg_white">
+                <svg x="0px" y="0px" viewBox="0 186.5 1920 113.5">
+                    <polygon points="0,300 655.167,210.5 1432.5,300 1920,198.5 1920,300 "></polygon>
+                </svg>
+            </div>
+        </section>
+    )
+}
+
+export default About;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import '../styles/globals.css'
+import 'font-awesome/css/font-awesome.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/flaticon.css'
+import '../styles/style.css'
+import '../styles/Responsive.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
